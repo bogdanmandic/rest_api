f9a4b2c97090a2ab2d96ef31adf2c015d5edc3dd
bogdanmandic <bogdan.mandic@cadev.rs>
2017-11-06 15:57:04 +0100

    REST API /w NODE

---
+++ models/movie.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var MovieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    poster: {
+        type: String,
+        default: "http://via.placeholder.com/350x150",
+        required: true
+    },
+    genre: {
+        type: String,
+        enum: [
+            'Comedy', 'Romance', 'Thriller', 'Horror', 'SciFi', 'Cartoon', 'Action'
+        ],
+    },
+    duration: Number,
+    releaseDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
